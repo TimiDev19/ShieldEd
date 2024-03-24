520cc715266158e9a4e3ecd9a9ee655e01bbe8d2
Nusrah00 <nusratajoke@gmail.com>
2024-03-24 15:37:08 +0100

    Added second button in learingCard

--- src/pages/learningCard.js
+++ src/pages/learningCard.js
@@ -1,15 +1,23 @@
 import '../styles/learningCard.css';
+import WhatshotOutlinedIcon from '@mui/icons-material/WhatshotOutlined';
 
 const learningCard = () => {
   
     return (
         <div className="App">
-      
-        
+             
               <div className='learning'>
               <button className='btn'>UNIT 1</button>
               <h4 className="text">Common Phishing Techniques</h4>
               <p className="text-two">Before you begin the challenge, let's learn about common tactics used by <br/>cybercriminals in phishing attacks.</p>
+              </div>
+              
+              <div>
+              
+              </div>
+              
+              <div className='secondBtn'>
+              <button className='btn2'>Start challenge <h4><WhatshotOutlinedIcon className='icons' /></h4></button>
               </div>
             
         </div>
@@ -19,11 +27,3 @@ const learningCard = () => {
 
 export default learningCard;
 
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-  
-// }
-
-// export default App;
