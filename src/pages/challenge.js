c8ea0e47d00e33614d5d2063a6159fee7aec05e4
winifred29 <winifreddd29@gmail.com>
2024-03-24 09:23:47 +0100

    additon:challenge page

---
+++ src/pages/challenge.js
@@ -0,0 +1,22 @@
+import heartIcon from '../assets/heartIcon.png' 
+
+const challenges = () => {
+  return (
+    <div className="challengesContainer">
+      <div className="progressBar">
+        <div className="progress">
+          <div className="progressText">1/5</div>
+        </div>
+        <img src={heartIcon}/>
+      </div>
+
+      <p>Identifying Common Phishing Techniques</p>
+      <h3>
+        A suspicious link has landed in your inbox. Is it a harmless URL or a
+        phishing trap? Analyze the email carefully and make your decision
+      </h3>
+    </div>
+  );
+};
+
+export default challenges;
