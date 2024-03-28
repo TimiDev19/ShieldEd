9b2466c4af6fc40f53ce47aae74c3ba66b63031d
Nusrah00 <nusratajoke@gmail.com>
2024-03-28 22:30:20 +0100

    question page work in progress

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,12 +7,14 @@ import sectionThreeIconTwo from "../assets/sectionThreeIcontwo.png";
 const LandingPage = () => {
   return (
     <div>
+    <div className="learningBody">
       <div className="sectionThree">
+      
         <div className="leftSide">
           <div className="leftIcon">
             <img src={leftIcon} />
           </div>
-          <h4>We Keep You Motivated</h4>
+          <h3>We Keep You Motivated</h3>
           <p className="smallText">
             With game-like features, interactive challenges, and <br /> friendly
             reminders, we ensure that learning adventure.
@@ -37,6 +39,29 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
+      
+      
+      <div className="cards">
+      <h3>Have Questions? We're Happy to Help!</h3>
+      
+<div class="card-container">
+  <div class="column">
+    <div class="card"><p>Is ShieldEd suitable for beginners?</p>
+<p>Yes, ShieldEd is suitable for beginners and users of all skill levels. The platform offers introductory courses covering fundamental cybersecurity concepts in an accessible and easy-to-understand manner.  ShieldEd's user-friendly interface and interactive learning approach make it ideal for individuals who are new to cybersecurity and want to build a solid foundation of knowledge.
+Security and Privacy
+Community  Features</p></div>
+    <div class="card">Security and Privacy</div>
+    <div class="card">Community Features</div>
+  </div>
+  <div class="column">
+    <div class="card">Team Training</div>
+    <div class="card">Technical Support</div>
+    <div class="card">Subscription and Pricing</div>
+
+  </div>
+</div>
+      </div>
+    </div>
     </div>
   );
 };
