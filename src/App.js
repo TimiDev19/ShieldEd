db51d9c8219a147633376b798fcebc99ac79a94d
ngochibabe <NgoziOgalue@Womentechster.org>
2024-04-08 23:32:31 +0100

    leaderboard

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import unitCompletion from './pages/unitCompletion.js';
 import getStarted from './pages/getStarted.js'
 import { lazy } from 'react';
 import successPage from './pages/successPage.js';
+import leaderboard from './pages/leaderboard.js';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Routes>
         <Route path='/' exact Component={LandingPage} />
         <Route path="/dashboard" exact Component={Dashboard}/>
+        <Route path="/leaderboard" exact Component={leaderboard}/>
         <Route path="/learningCard" exact Component={learningCard}/>
         <Route path="/challenges" exact Component={challenges}/>
         <Route path="/courses" exact Component={coursePage} />
